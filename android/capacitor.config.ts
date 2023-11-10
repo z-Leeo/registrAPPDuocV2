53d8cb89900e7821a0b77217f94080abd19b8168
z-Leeo <le.montenegro@duocuc.cl>
2023-11-10 19:08:21 -0300

    commit 10

---
+++ android/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.local.registrappduocv2',
+  appName: 'com.local.registrappduocv2',
+  webDir: 'com.local.registrappduocv2',
+  bundledWebRuntime: false
+};
+
+export default config;
